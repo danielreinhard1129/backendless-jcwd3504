345d3cc694353a66be33f49ae3fb9e7c76ede9bc
Daniel Reinhard <119338658+danielreinhard1129@users.noreply.github.com>
2025-11-27 23:58:20 +0700

    Add console log to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  console.log("hello");
   return (
     <main>
       <Button variant="destructive">Button</Button>
