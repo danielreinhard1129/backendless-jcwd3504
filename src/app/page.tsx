e07bf931ae3b3ffbec541ae24ca16f0760d00415
Daniel Reinhard <119338658+danielreinhard1129@users.noreply.github.com>
2025-11-28 00:19:01 +0700

    Remove console.log statement from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  console.log("hello");
   return (
     <main>
       <Button variant="destructive">Button</Button>
